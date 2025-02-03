1546c911dfb63fe0978c8f75394a28b66264be03
Pranav <raramuri@raramuri-MacBook-Pro.local>
2025-02-03 16:47:31 +0530

    Improved the design

--- src/Home.tsx
+++ src/Home.tsx
@@ -41,16 +41,16 @@ const Home = () => {
 
       <Heading
         heading={"Master the useReducer Hook"}
-        headingcolor={"#105652"}
+        headingcolor={"#1B3C73"}
         children={
           <>
-            <p style={{ color: "#47A992", fontWeight: "bold" }}>
+            <p style={{ color: "#0A97B0", fontWeight: "bold" }}>
               🎭 Think of Your App as a Theater Play 🎭
             </p>
             <p><TbCircleNumber1Filled style={{fontSize:'1.3rem'}}/> Actors (Components) perform actions.</p>
             <p><TbCircleNumber2Filled style={{fontSize:'1.3rem'}}/> A Script (Reducer) decides what happens.</p>
             <p><TbCircleNumber3Filled style={{fontSize:'1.3rem'}}/> The Director (useReducer) manages the play.</p>
-            <p style={{ color: "#279EFF" }}>
+            <p style={{ color: "#0A97B0" }}>
               Instead of using <b>useState</b> to manage state directly, <b>useReducer</b> acts as a
               centralized manager that updates the state based on defined rules (reducer function).
             </p>
@@ -60,20 +60,20 @@ const Home = () => {
 
       <Heading
         heading={"Syntax"}
-        headingcolor={"#144272"}
+        headingcolor={"#1B3C73"}
         children={
           <>
             <img src="syntax.png" alt="syntax" className="syntax-image" />
-            <p style={{ color: "#500073" }}>
+            <p>
             <TbCircleNumber1Filled style={{fontSize:'1.3rem'}}/> <b>reducer</b>: A function that determines state transitions based on an action.
             </p>
-            <p style={{ color: "#500073" }}>
+            <p>
             <TbCircleNumber2Filled style={{fontSize:'1.3rem'}}/> <b>initialState</b>: The initial value of the state.
             </p>
-            <p style={{ color: "#500073" }}>
+            <p>
             <TbCircleNumber3Filled style={{fontSize:'1.3rem'}}/> <b>state</b>: The current state.
             </p>
-            <p style={{ color: "#500073" }}>
+            <p>
             <TbCircleNumber4Filled style={{fontSize:'1.3rem'}}/> <b>dispatch</b>: A function to send actions to the reducer.
             </p>
           </>
@@ -82,19 +82,19 @@ const Home = () => {
 
       <Heading
         heading={"useReducer Vs useState 🤔"}
-        headingcolor={"#2E79BA"}
+        headingcolor={"#1B3C73"}
         children={
           <>
-            <p style={{ color: "#37AFE1" }}>🗿 Clearer State Management - All state updates follow a single function.</p>
-            <p style={{ color: "#37AFE1" }}>🗿 Better for Complex Logic - No need for multiple useState calls.</p>
-            <p style={{ color: "#37AFE1" }}>🗿 Predictable State Updates - Changes happen in a controlled manner.</p>
+            <p>🗿 Clearer State Management - All state updates follow a single function.</p>
+            <p>🗿 Better for Complex Logic - No need for multiple useState calls.</p>
+            <p>🗿 Predictable State Updates - Changes happen in a controlled manner.</p>
 
-            <p style={{ color: "#37AFE1" }}>✅ If your component has simple state, useState is fine.</p>
-            <p style={{ color: "#37AFE1" }}>✅ If your component has multiple related states or complex updates, useReducer is better!</p>
+            <p>✅ If your component has simple state, useState is fine.</p>
+            <p>✅ If your component has multiple related states or complex updates, useReducer is better!</p>
           </>
         }
       />
-      <Heading heading={"Example"} headingcolor={"#500073"} children={
+      <Heading heading={"Example"} headingcolor={"#1B3C73"} children={
         <>
         <Counter/>
         <button  onClick={() => setShowimage((prev) => !prev)}>
